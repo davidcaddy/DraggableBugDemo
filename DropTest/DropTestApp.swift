d8a592171ea9ca972cc3ea27b6cdc38c8940b665
David Caddy <dev@davidcaddy.com>
2024-06-16 21:43:36 +1000

    Initial Commit

---
+++ DropTest/DropTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropTestApp.swift
+//  DropTest
+//
+//  Created by David Caddy on 16/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct DropTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
